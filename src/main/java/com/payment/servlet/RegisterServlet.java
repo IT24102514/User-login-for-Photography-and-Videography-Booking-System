fa12e0db9f0bcf50a2935fdce3ae989ee0a6b018
IT24102514 <IT24102514@my.sliit.lk>
2025-05-21 15:40:49 +0530

    new one RegisterServlet.java

--- src/main/java/com/payment/servlet/RegisterServlet.java
+++ src/main/java/com/payment/servlet/RegisterServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+//URL maping
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
     private UserDAO userDAO = new UserDAO();
