182c923a76814fd89bae1b444041ad1f6076d102
IT24102514 <IT24102514@my.sliit.lk>
2025-05-21 15:48:15 +0530

    Update UpdateAvailabilityServlet.java

--- src/main/java/com/payment/servlet/UpdateAvailabilityServlet.java
+++ src/main/java/com/payment/servlet/UpdateAvailabilityServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
+//URL maping
 @WebServlet("/update-availability")
 public class UpdateAvailabilityServlet extends HttpServlet {
     private UserDAO userDAO = new UserDAO();
